b9e123c610809a51047cea0a4d2d88234d31936c
Gazaunga <Gazaunga@gmail.com>
2018-01-23 22:04:27 -0500

    Update mkruby.rb

--- mkruby.rb
+++ mkruby.rb
@@ -1,5 +1,7 @@
 #!/usr/bin/env ruby
 
+require 'fileutils'
+
 filename = ARGV[0]
 language = ARGV[1]
 
@@ -7,6 +9,8 @@ File.open("#{filename}.rb", "a+") do |f|
 script = <<-EOS
 #!/usr/bin/env #{language}
 
+
 EOS
 f.write(script)
+# FileUtils.chmod "#{filename}.rb", "0755", :verbose => true 
 end
