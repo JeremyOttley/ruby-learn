3dfeba6b8c24dcaa069267e214053989372ad350
Gazaunga <Gazaunga@gmail.com>
2018-03-11 13:37:15 -0400

    Create utils.rb

---
+++ utils.rb
@@ -0,0 +1,18 @@
+#!/usr/bin/env ruby
+
+#require 'fileutils'
+#require 'pathname'
+
+##=UTILS=##
+
+def chdir(dir)
+
+puts Dir.pwd
+
+Dir.chdir(dir) do
+puts Dir.pwd
+end
+
+end
+
+#=> chdir("lib/")
