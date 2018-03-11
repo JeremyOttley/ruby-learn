b93f4b8b011084ce018f0a5d5af4888bba31d636
Gazaunga <Gazaunga@gmail.com>
2018-03-11 14:47:13 -0400

    Update app.rb

--- toml/app.rb
+++ toml/app.rb
@@ -5,8 +5,8 @@ require 'pp'
 
 path = File.join(File.dirname(__FILE__), "config/config.toml")
 
-obj = TOML.load_file(path)
+config = TOML.load_file(path)
 
-obj.each { |k,v| puts "Current #{k} is #{v}" }
+config.each { |k,v| puts "Current #{k} is #{v}" }
 
-#pp obj
+#pp config
