27514ee71c304d07e2edcd7884f61d767e0e2a6a
Gazaunga <Gazaunga@gmail.com>
2018-03-28 20:39:23 -0400

    Create deploy.rb

---
+++ deploy.rb
@@ -0,0 +1,15 @@
+#!/usr/bin/env ruby
+
+require 'fileutils'
+
+SOURCE = gets.chomp
+
+DESTINATION = "#{Dir.home}/"
+
+Dir.glob("#{Dir.home}/#{SOURCE}/.", File::FNM_DOTMATCH).each { |f| FileUtils.cp_r("#{f}", DESTINATION, :verbose => true) }
+
+#input example: /test/
+
+
+
+
