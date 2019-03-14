17989849066e460a2aa9f13466c504df9cf45597
Jeremy Ottley <Gazaunga@gmail.com>
2019-03-13 21:04:06 -0400

    Update stock.rb

--- stock.rb
+++ stock.rb
@@ -5,8 +5,7 @@ while true do
   up_arrow = "\u2191".colorize(:green)
   down_arrow = "\u2193".colorize(:red)
 
-  stock_data_hash = IEX::Resources::Quote.get('CRON')
-  stock_change = stock_data_hash['change'].to_s
+  stock_change = IEX::Resources::Quote.get('CRON')['change'].to_s
 
 if stock_change.include?("-") == true
   puts down_arrow + stock_change
