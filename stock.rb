a18cb786adf2675dd3034a5c22eeb61c52abf559
Jeremy Ottley <Gazaunga@gmail.com>
2019-03-13 21:00:22 -0400

    Update stock.rb

--- stock.rb
+++ stock.rb
@@ -2,19 +2,15 @@ require 'colorize'
 require 'iex-ruby-client'
 
 while true do
+  up_arrow = "\u2191".colorize(:green)
+  down_arrow = "\u2193".colorize(:red)
 
-up_arrow = "\u2191".colorize(:green)
-
-down_arrow = "\u2193".colorize(:red)
-
-stock_data_hash = IEX::Resources::Quote.get('CRON')
-
-stock_change = stock_data_hash['change'].to_s
+  stock_data_hash = IEX::Resources::Quote.get('CRON')
+  stock_change = stock_data_hash['change'].to_s
 
 if stock_change.include?("-") == true
   puts down_arrow + stock_change
 elsif stock_change.include?("+") == true
   puts up_arrow + stock_change
 end
-
 end
