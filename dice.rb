e8ae6976be6a21f322f51231b879e7d3cc6b209e
Jeremy Ottley <Gazaunga@gmail.com>
2019-03-27 14:13:33 -0400

    Create dice.rb

---
+++ dice.rb
@@ -0,0 +1,21 @@
+require 'ostruct'
+
+die_face = OpenStruct.new(
+  :face_1 => "\u2680",
+  :face_2 => "\u2681",
+  :face_3 => "\u2682",
+  :face_4 => "\u2683",
+  :face_5 => "\u2684",
+  :face_6 => "\u2685"
+)
+
+dice  = []
+
+dice << die_face[:face_1]
+dice << die_face[:face_2]
+dice << die_face[:face_3]
+dice << die_face[:face_4]
+dice << die_face[:face_5]
+dice << die_face[:face_6]
+
+puts dice
